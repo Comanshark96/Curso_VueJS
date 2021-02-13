25756bd4ce5e5346ee10032d9cc1b8178758ab4f
Carlos_Rivera <carlos.rivera.munoz@disr.it>
2021-02-13 01:24:02 -0600

    04 Clases (v-bind:class)

--- app.js
+++ app.js
@@ -1,31 +1,9 @@
 const app = new Vue({
   el: '#app',
   data: {
-    titulo: 'Hola mundo con Vue',
-    frutas: [
-      {nombre: 'Pera', cantidad: 10},
-      {nombre: 'Manzana', cantidad: 5},
-      {nombre: 'Banana', cantidad: 0}
-    ],
-    nuevaFruta: '',
-    totalFrutas: 0,
+    fondo: 'bg-warning',
+    color: false
   },
   methods: {
-    agregarFruta() {
-      this.frutas.push({
-	nombre: this.nuevaFruta, cantidad: 0
-      });
-      this.nuevaFruta = '';
-    },
   },
-  computed: {
-    sumarFrutas() {
-      this.totalFrutas = 0;
-      for(fruta of this.frutas){
-	this.totalFrutas += fruta.cantidad;
-      }
-      
-      return this.totalFrutas
-    }
-  }
 })
