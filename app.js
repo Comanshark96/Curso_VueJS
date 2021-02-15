a3aa075f9d5b1ef5e5a1028b1e7720ea56e63c0f
Carlos_Rivera <carlos.rivera.munoz@disr.it>
2021-02-15 00:04:39 -0600

    09 Comunicación entre componentes (de hijo a padre)

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ Vue.component('padre', {
   template:`
   <div class="text-center p-5 bg-dark text-white">
     <h2>Componente padre: {{ numero }}</h2>
+    <h3>Tien por hijo a «{{ nombrePadre }}»</h3>
     <button class="btn btn-success" @click="numero++">+</button>
-    <hijo :numero="numero"></hijo>
+    <hijo :numero="numero" @nombreHijo="nombrePadre = $event"></hijo>
   </div>
   `,
   data() {
-    return {numero: 0}
+    return {
+      numero: 0,
+      nombrePadre: ''
+    }
   }
 })
   
@@ -15,9 +19,18 @@ Vue.component('hijo', {
   template:`
   <div class="m-2 text-white text-center bg-danger">
     <h3>Componente hijo: {{ numero }}</h3>
+    <h4>Nombre: {{ nombre }}</h4>
   </div>
   `,
-  props: ['numero']
+  props: ['numero'],
+
+  data() {
+    return {nombre: 'Julio'}
+  },
+  
+  mounted() {
+    this.$emit('nombreHijo', this.nombre)
+  }
 })
 
 const app = new Vue({
