3bf2be864cbba938b6e5410a1908e0e0e662a4e4
Carlos_Rivera <carlos.rivera.munoz@disr.it>
2021-02-14 23:44:24 -0600

    08 Comunicación entre componentes (de padre a hijo)

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
-Vue.component('saludo', {
-  template: '<h1>{{ saludo }}</h1>',
+Vue.component('padre', {
+  template:`
+  <div class="text-center p-5 bg-dark text-white">
+    <h2>Componente padre: {{ numero }}</h2>
+    <button class="btn btn-success" @click="numero++">+</button>
+    <hijo :numero="numero"></hijo>
+  </div>
+  `,
   data() {
-    return {saludo: 'Hola desde componente Vue'}
+    return {numero: 0}
   }
 })
   
+Vue.component('hijo', {
+  template:`
+  <div class="m-2 text-white text-center bg-danger">
+    <h3>Componente hijo: {{ numero }}</h3>
+  </div>
+  `,
+  props: ['numero']
+})
+
 const app = new Vue({
   el: '#app',
   data: {
